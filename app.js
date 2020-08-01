2c6b582bd48deaaab39f8ebb4d6029274e3e709d
Akash <akashorasad2000@gmail.com>
2020-08-01 18:02:37 +0530

    added unhandled expception and unhandled rejection in the app

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 const express = require('express');
-const morgan = require('morgan')
-
+const morgan = require('morgan');
 
 const productsRouter = require('./routes/productRoutes');
 const searchController = require('./controllers/searchController');
-const globalErrorHandler = require('./controllers/errorController')
+const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
 const app = express();
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-
-app.use('/api/v1/products', productsRouter)
+app.use('/api/v1/products', productsRouter);
 
 //----SEARCH-----//
-app.use('/api/v1/search', searchController.search)
+app.use('/api/v1/search', searchController.search);
 
 app.all('*', (req, res, next) => {
-
-    next(new AppError(`Can't find ${req.originalUrl} on the server !.`, 404));
-})
+  next(new AppError(`Can't find ${req.originalUrl} on the server !.`, 404));
+});
 
 app.use(globalErrorHandler);
-
 
 // Exports express app to the server.js file
 module.exports = app;
--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  const { name, message } = err;
+  console.log({ name, message });
+});
+
 dotenv.config({
-    path: './config.env'
+  path: './config.env',
 });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 
-
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
-
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => console.log('DB CONNECTED SUCCESSFULY'))
+    useCreateIndex: true,
+  })
+  .then(() => console.log('DB CONNECTED SUCCESSFULY'));
 
-const port = process.env.PORT || 90
-app.listen(port, () => {
-    console.log(`Connected to port ${port} 💜`)
+const port = process.env.PORT || 90;
+const server = app.listen(port, () => {
+  console.log(`Connected to port ${port} 💜`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log({ name: err.name, message: err.message }, err);
+  console.log('UNHANDLED_REJECTION, SHUTTING DOWN SERVER !!');
+  server.close();
 });
